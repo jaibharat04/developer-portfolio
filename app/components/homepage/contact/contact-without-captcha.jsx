3e806f52b0558ac3601281c065c5c0750fb341c1
Jai Bharat <123252880+jaibharat04@users.noreply.github.com>
2024-09-04 16:30:00 +0530

    Update contact-without-captcha.jsx

--- app/components/homepage/contact/contact-without-captcha.jsx
+++ app/components/homepage/contact/contact-without-captcha.jsx
@@ -70,6 +70,7 @@ function ContactWithoutCaptcha() {
               className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
               type="text"
               maxLength="100"
+              name= "to_name"
               required={true}
               onChange={(e) => setInput({ ...input, name: e.target.value })}
               onBlur={checkRequired}
@@ -83,6 +84,7 @@ function ContactWithoutCaptcha() {
               className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
               type="email"
               maxLength="100"
+              name= "from_name"
               required={true}
               value={input.email}
               onChange={(e) => setInput({ ...input, email: e.target.value })}
